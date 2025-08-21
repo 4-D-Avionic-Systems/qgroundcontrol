8e0b49669bc78792030dede60edeb9fcba239bdf
Mason Phillips <mason@4davionicsystems.com>
2025-08-21 15:59:39 -0500

    Added lines to automatically start the 4DAS API when qgc is started (they will have to be uncommented and modified to match your implementation of qgc)

--- src/main.cc
+++ src/main.cc
@@ -190,6 +190,10 @@ int main(int argc, char *argv[])
 #endif
 #endif
 
+//----------------------4DAS Changes----------------------
+    //system("dotnet build ~/code/CSharp_ConflictDetection_Resolution_MVP/CDR_MVP/ &");
+    //system("ASPNETCORE_URLS=http://localhost:5248 dotnet exec ~/code/CSharp_ConflictDetection_Resolution_MVP/CDR_MVP/CDR_MVP/bin/Debug/net8.0/CDR_MVP.dll &");
+
     app.init();
 
     // Set system ID if specified via command line, for example --system-id:255
