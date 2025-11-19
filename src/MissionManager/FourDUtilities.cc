7ff6c6377b820acd8c139bd7f9c31e386aa3a898
Carrie Aponte <111673954+Carrie-Aponte@users.noreply.github.com>
2025-11-19 09:15:42 -0600

    fixing types issue

--- src/MissionManager/FourDUtilities.cc
+++ src/MissionManager/FourDUtilities.cc
@@ -131,7 +131,7 @@ QNetworkReply* FourDUtilities::detectConflictSingleGeoFencePolygon(FourDRequestB
 
 QNetworkReply* FourDUtilities::overwriteGeoFences(QJsonDocument geoFences){
     QJsonObject geoFenceObj = geoFences.object();
-    geoFenceObj["customerId"] = _getCustomerID();
+    geoFenceObj["customerId"] = QJsonValue::fromVariant(_getCustomerID());
     QJsonDocument geoFencesWithCustomerId(geoFenceObj);
     
     QUrl post_url = _apiUrl.resolved(QUrl("/GeoFence/Overwrite"));
@@ -143,7 +143,7 @@ QNetworkReply* FourDUtilities::overwriteGeoFences(QJsonDocument geoFences){
 
 QNetworkReply* FourDUtilities::addGeoFences(QJsonDocument geoFences){
     QJsonObject geoFenceObj = geoFences.object();
-    geoFenceObj["customerId"] = _getCustomerID();
+    geoFenceObj["customerId"] = QJsonValue::fromVariant(_getCustomerID());
     QJsonDocument geoFencesWithCustomerId(geoFenceObj);
     
     QUrl post_url = _apiUrl.resolved(QUrl("/GeoFence/Add"));
